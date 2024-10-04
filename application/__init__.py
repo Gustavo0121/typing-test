03f8991629a7f4cc55b247cd3c3bcef9f7e268b6
Gustavo dos Santos Ribeiro <gustavo.ribeiro@presidencia.gov.br>
2024-10-04 14:56:42 -0300

    tests: Configuração para ambiente de testes

---
+++ application/__init__.py
@@ -0,0 +1,12 @@
+"""Main module."""
+
+from pathlib import Path
+import logging
+
+__title__ = '.'.join(Path(__file__).parent.parts[-2:])
+
+confproject = Path(__file__).parents[1] / 'pyproject.toml'
+versionfile = Path(__file__).parent / 'version.txt'
+
+if __name__ == '__main__':  # pragma: no cover
+    logging.info('(%s)', __title__)
