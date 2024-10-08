e34f40a67599ef5de11e047922404c7a20581245
Gustavo dos Santos Ribeiro <gustavo.ribeiro@presidencia.gov.br>
2024-10-08 15:57:59 -0300

    refactor: poema substituído por frases para digitação

--- application/view/typing.py
+++ application/view/typing.py
@@ -14,41 +14,36 @@ class Typing(ft.View):
         self.horizontal_alignment = ft.CrossAxisAlignment.CENTER
         self.padding = 50
         self.bgcolor = '#181717'
-        self.text = """
-Shall I compare thee to a summer's day?
-Thou art more lovely and more temperate:
-Rough winds do shake the darling buds of May,
-And summer's lease hath all too short a date;
-Sometime too hot the eye of heaven shines,
-And often is his gold complexion dimm'd;
-And every fair from fair sometime declines,
-By chance or nature's changing course untrimm'd;
-But thy eternal summer shall not fade,
-Nor lose possession of that fair thou ow'st;
-Nor shall death brag thou wander'st in his shade,
-When in eternal lines to time thou grow'st:
-So long as men can breathe or eyes can see,
-So long lives this, and this gives life to thee
-                            """.lower().strip()
+        self.text = """You must be the change you wish to see in the world.""".lower().strip()
         self.campo_texto = ft.TextField(
             autofocus=True,
             border=ft.InputBorder.NONE,
-            text_size=100,
+            text_size=60,
             multiline=True,
             on_change=self.on_change,
             color='#890606',
+            max_lines=1,
         )
         self.controls = [
+            ft.Text('0:00', color='#A40000', size=60),
             ft.Stack(
                 controls=[
-                    ft.Text(
-                        self.text,
-                        color=ft.colors.with_opacity(0.3, '#890606'),
-                        size=100,
+                    ft.Container(
+                        content=ft.Row(
+                            controls=[
+                                ft.Text(
+                                    self.text,
+                                    color=ft.colors.with_opacity(0.3, '#890606'),
+                                    size=60,
+                                    style=ft.TextStyle(word_spacing=1)
+                                ),
+                            ],
+                        ),
+                        padding=ft.padding.all(2),
                     ),
                     ft.Container(
                         content=self.campo_texto,
-                        offset=ft.Offset(x=0, y=-0.05)
+                        offset=ft.Offset(x=0, y=-0.04),
                     ),
                 ],
             ),
